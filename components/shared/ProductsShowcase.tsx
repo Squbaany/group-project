da0045a508261b4b58b9498c648c9ec159921ab8
Igor <igorpacholski@gmail.com>
2024-06-07 21:51:55 +0200

    image size change

--- components/shared/ProductsShowcase.tsx
+++ components/shared/ProductsShowcase.tsx
@@ -16,7 +16,7 @@ const ProductsShowcase = ({ products }: Props) => {
     );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
       {products.map((product) => (
         <Link
           href={`/product/${product._id}`}
